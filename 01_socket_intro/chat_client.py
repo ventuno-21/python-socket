daa85c51b1792136224abfc92cace2009914beec
ventuno-21 <yasna.pakdel@outlook.com>
2025-03-26 09:14:36 -0700

    02-07 Basic two-way chat enabling the chat

--- 01_socket_intro/chat_client.py
+++ 01_socket_intro/chat_client.py
@@ -10,3 +10,21 @@ BYTE_SIZE = 1024
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect((DEST_IP, DEST_PORT_NO))
+
+
+# send & receive messages
+while True:
+    # receive info from server
+    message = client_socket.recv(BYTE_SIZE).decode(ENCODER)
+    # Quit if the connected server wants to quit, else keep sending messages
+    if message == "quit":
+        client_socket.send("quit".encode(ENCODER))
+        print("\n client: Enging the chat .. bye")
+        break
+    else:
+        print(f"\n{message}")
+        new_message = input("client message: ")
+        client_socket.send(new_message.encode(ENCODER))
+
+# if we write 'quit' we will break a while loop then we should close the connection
+client_socket.close()
--- 01_socket_intro/chat_server.py
+++ 01_socket_intro/chat_server.py
@@ -22,7 +22,7 @@ server_socket.listen()
 
 
 # Accept any incoming connection and inform them
-print("Server is running .... \n")
+print("Server is running & listening .... \n")
 
 # accepet any single connection & store two pieces of info
 client_socket, client_address = server_socket.accept()
@@ -32,3 +32,22 @@ print(client_socket)
 print(client_address)
 
 client_socket.send("server: You are connected ...\n".encode(ENCODER))
+
+
+# send/recieve messages
+while True:
+    # receive infor from client
+    message = client_socket.recv(BYTE_SIZE).decode(ENCODER)
+
+    # quite if client socket wants to quit, otherwise show messages
+    if message == "quit":
+        client_socket.send("quit".encode(ENCODER))
+        print("\n server: Ending the chat .. bye")
+        break
+    else:
+        print(f"\n {message}")
+        message = input("server message: ")
+        client_socket.send(message.encode(ENCODER))
+
+
+server_socket.close()
