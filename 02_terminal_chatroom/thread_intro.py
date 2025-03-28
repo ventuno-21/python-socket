babbc40689d50c3800bb6b7aa2eb2daf511cc920
ventuno-21 <yasna.pakdel@outlook.com>
2025-03-28 08:44:15 -0700

    03-02 Threading module basic

--- 02_terminal_chatroom/thread_intro.py
+++ 02_terminal_chatroom/thread_intro.py
@@ -26,7 +26,36 @@ def function_3():
         print("three")
 
 
-# below functions will be run linearly
-function_1()
-function_2()
-function_3()
+# # below functions will be run linearly/sequentially
+# function_1()
+# function_2()
+# function_3()
+
+
+# execute these functions concurently using threads
+t1 = threading.Thread(target=function_1)
+t2 = threading.Thread(target=function_2)
+t3 = threading.Thread(target=function_3)
+
+t1.start()
+t2.start()
+t3.start()
+
+t1.join()
+t2.join()
+t3.join()
+
+# threading can only be run once, if you cant to reuse, you must redeifine
+t1 = threading.Thread(target=function_1)
+t1.start()
+t1.join()
+
+
+# if you want to pause the main program until a thread is done you can
+t1 = threading.Thread(target=function_1)
+t1.start()
+t1.join()
+
+print(
+    "if we use join() method, this line will be execute after that, otherwise if we dont use join(), this print will show up anytime between our tasks's outputs"
+)
