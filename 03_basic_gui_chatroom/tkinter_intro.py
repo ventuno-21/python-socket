4dddcd02288c0bc8cd5c205267148c01858e208e
ventuno-21 <yasna.pakdel@outlook.com>
2025-03-28 22:57:19 -0700

    04-03 Tkinter module - Adding widgets

--- 03_basic_gui_chatroom/tkinter_intro.py
+++ 03_basic_gui_chatroom/tkinter_intro.py
@@ -1,4 +1,4 @@
-from tkinter import BOTH, END, Tk
+from tkinter import BOTH, StringVar, END, Tk
 import tkinter
 
 # define a window
@@ -35,6 +35,30 @@ send_button = tkinter.Button(input_frame, text="Send", bg=output_color)
 message_entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 send_button.grid(row=0, column=3, rowspan=2, padx=10, pady=10, ipadx=20, ipady=5)
 
+# because all 3 buttons are linked to text_color, only one of them can be chosen
+text_color = StringVar()
+text_color.set("#ff0000")  # only one radiobutton will be chosen
+red_button = tkinter.Radiobutton(
+    input_frame, text="Red", variable=text_color, value="#ff0000", bg=input_color
+)
+green_button = tkinter.Radiobutton(
+    input_frame, text="Green", variable=text_color, value="#00ff00", bg=input_color
+)
+blue_button = tkinter.Radiobutton(
+    input_frame, text="Blue", variable=text_color, value="#0000ff", bg=input_color
+)
+red_button.grid(row=1, column=0)
+green_button.grid(row=1, column=1)
+blue_button.grid(row=1, column=2)
+
+output_label = tkinter.Label(
+    output_frame,
+    text="--- Stored Messages ---",
+    fg=input_color,
+    bg=output_color,
+    font=("Helvetica bold", 10),
+)
+output_label.pack(pady=15)
 
 # run the root window's mainloop
 root.mainloop()
