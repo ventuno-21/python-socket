fb05b63a9be1dc4cf072bf2101c1cdfd118d0b28
ventuno-21 <yasna.pakdel@outlook.com>
2025-03-29 06:24:09 -0700

    04-04 Tkinter module - adding functionality

--- 03_basic_gui_chatroom/tkinter_intro.py
+++ 03_basic_gui_chatroom/tkinter_intro.py
@@ -12,12 +12,30 @@ root.resizable(0, 0)  # it means you are not allow to resize it
 
 
 # Define colors
-root_color = "#e4cfa1"
-input_color = "#4f646f"
-output_color = "#dee7e7"
+root_color = "#F8EBDE"
+# input_color = "#4f646f"
+input_color = "#EAECC6"
+# output_color = "#dee7e7"
+output_color = "#FEFBF8"
 root.config(bg=root_color)
 
-# define functions
+
+# Define functions
+def send_message():
+    """Send the users message to the output frame"""
+    message_label = tkinter.Label(
+        output_frame,
+        text=message_entry.get(),
+        fg=text_color.get(),
+        bg=output_color,
+        font=("Helvetica", 12),
+    )
+    message_label.pack()
+
+    # Clear the entry field for the next message/autodelete
+    # Form index (0 to end ) will be deleted from our message_entry input after
+    # press send button
+    message_entry.delete(0, END)
 
 
 # Define Gui layout
@@ -31,23 +49,25 @@ output_frame.pack(padx=10, pady=(0, 10), fill=BOTH, expand=True)
 message_entry = tkinter.Entry(
     input_frame, text="Enter a message", width=25, font=("Helvetica", 12)
 )
-send_button = tkinter.Button(input_frame, text="Send", bg=output_color)
+send_button = tkinter.Button(
+    input_frame, text="Send", bg=output_color, command=send_message
+)
 message_entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 send_button.grid(row=0, column=3, rowspan=2, padx=10, pady=10, ipadx=20, ipady=5)
 
 # because all 3 buttons are linked to text_color, only one of them can be chosen
 text_color = StringVar()
 text_color.set("#ff0000")  # only one radiobutton will be chosen
-red_button = tkinter.Radiobutton(
-    input_frame, text="Red", variable=text_color, value="#ff0000", bg=input_color
+orange_button = tkinter.Radiobutton(
+    input_frame, text="Orange", variable=text_color, value="#FDAE73", bg=input_color
 )
 green_button = tkinter.Radiobutton(
-    input_frame, text="Green", variable=text_color, value="#00ff00", bg=input_color
+    input_frame, text="Green", variable=text_color, value="#98AB59", bg=input_color
 )
 blue_button = tkinter.Radiobutton(
-    input_frame, text="Blue", variable=text_color, value="#0000ff", bg=input_color
+    input_frame, text="Blue", variable=text_color, value="#1EB6BE", bg=input_color
 )
-red_button.grid(row=1, column=0)
+orange_button.grid(row=1, column=0)
 green_button.grid(row=1, column=1)
 blue_button.grid(row=1, column=2)
 
